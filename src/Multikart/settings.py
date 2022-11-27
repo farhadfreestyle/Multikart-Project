a8d07cc173cbc3d41fb3ff4a60f484388d326565
farhadfreestyle <agayevferhad02@gmail.com>
2022-11-27 19:41:37 +0400

    updated

--- src/Multikart/settings.py
+++ src/Multikart/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%9a5=j+bb^ar4_qfcnb-=m-2$#aun$c1pamm03^@8l8^nho6*s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
